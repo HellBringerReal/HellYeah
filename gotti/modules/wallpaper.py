72f5a12e18abc64abfe102953269e0f7a007f525
Lucifer Morningstar <58784327+HellBringerReal@users.noreply.github.com>
2020-08-28 02:20:58 +0530

    Update wallpaper.py

--- gotti/modules/wallpaper.py
+++ gotti/modules/wallpaper.py
@@ -7,8 +7,8 @@ from time import sleep
 from telegram import Message, Chat, Update, Bot
 from telegram.ext import run_async
 
-from cinderella import dispatcher,WALL_API
-from cinderella.modules.disable import DisableAbleCommandHandler
+from gotti import dispatcher,WALL_API
+from gotti.modules.disable import DisableAbleCommandHandler
 
 
 
@@ -27,7 +27,7 @@ def wall(bot: Bot, update: Update, args):
         term = query.replace(" ", "%20")
         json_rep = r.get(f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}").json()
         if not json_rep.get("success"):
-            msg.reply_text("An error occurred! Report this @cinderellabot")
+            msg.reply_text("An error occurred! Report this to @tubhiaaja")
         else:
             wallpapers = json_rep.get("wallpapers")
             if not wallpapers:
